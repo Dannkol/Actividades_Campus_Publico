3150a637ddc3cef57475f6dbf18f476950726be4
Daniel <manosalvasierra@gmail.com>
2023-02-16 18:57:54 -0500

    feat taller 1 punto 5

--- taller_1/main.py
+++ taller_1/main.py
@@ -1,11 +1,16 @@
 """
-    4. Que son las expresiones regulares en Python?
-
-    Las expresiones regulares son patrones que se utilizan para hacer coincidir combinaciones de caracteres en cadenas.
+    5. Enumere 5 tipos de datos en Python y suministre un valor de ejemplo de cada uno.
 """
 
-import re
+datoint = 1
+datopuntoflotante = 1.3
+datonumerocomplx = 1+2j
+datobool = False
+datostring1 = "Hola mundo"
 
-regx = re.compile(r'\bHola\b')  # busca la palabra foo con el delimitador \b   
 
-print(regx.match("Hola Mundo"))
+print(f"tenemos dato int {type(datoint)} = {datoint}")
+print(f"tenemos dato floar {type(datopuntoflotante)} = {datopuntoflotante}")
+print(f"tenemos dato complejo {type(datonumerocomplx)} en este dato tenemos su parte real {datonumerocomplx.real} y su parte imaginaria {datonumerocomplx.imag} = {datonumerocomplx}")
+print(f'tenemos datos bool {type(datobool)} = {datobool}')
+print(f'tenemos datos string {type(datostring1)} = {datostring1}')
