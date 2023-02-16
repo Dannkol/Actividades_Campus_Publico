181c143d9f1d4457540a761e7f24f7b258688c57
Daniel <manosalvasierra@gmail.com>
2023-02-16 18:57:19 -0500

    feat taller 1 punto 4

--- taller_1/main.py
+++ taller_1/main.py
@@ -1,1 +1,11 @@
-"""3. Si hay operadores de igual precedencia, en qué orden se ejecutan? De izquierda a derecha"""
+"""
+    4. Que son las expresiones regulares en Python?
+
+    Las expresiones regulares son patrones que se utilizan para hacer coincidir combinaciones de caracteres en cadenas.
+"""
+
+import re
+
+regx = re.compile(r'\bHola\b')  # busca la palabra foo con el delimitador \b   
+
+print(regx.match("Hola Mundo"))
