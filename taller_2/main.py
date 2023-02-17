914444e1854bd2027d05255c52652e0fdca35b8e
Daniel <manosalvasierra@gmail.com>
2023-02-17 02:07:52 -0500

    ERROR

--- taller_2/main.py
+++ taller_2/main.py
@@ -1,58 +1,136 @@
-ciclistas = {
-
+almacen = {
+  'pepe': {
+    'arroz': [
+      12,
+      1000
+    ],
+    'arroz2': [
+      10,
+      800
+    ],
+    'arroz3': [
+      9,
+      4000
+    ],
+    'arroz4': [
+      5,
+      2000
+    ],
+    'arroz5': [
+      12,
+      5000
+    ]
+  },
+  'juan': {
+    'arroz': [
+      12,
+      6000
+    ],
+    'arroz2': [
+      20,
+      10
+    ],
+    'arroz3': [
+      9,
+      4000
+    ],
+    'arroz4': [
+      5,
+      2000
+    ],
+    'arroz5': [
+      12,
+      5000
+    ]
+  },
+  'oi': {
+    '2': [
+      2,
+      2
+    ],
+    '4': [
+      2,
+      2
+    ],
+    '5': [
+      2,
+      2
+    ],
+    '6': [
+      2,
+      2
+    ],
+    '7': [
+      4,
+      8
+    ]
+  }
 }
-suldobacis = int(input("suldo basico por kilometro recorrido: "))
 
-total = 3277
 
-n = int(input("cuantos ciclistas participan: "))
+
+n = int(input("Cuantos amacenes creara?: "))
+
+p= {
+    
+}
+
 
 for i in range(n):
-    nombre = input("nombre del atleta: ")
-    estadisticas = []
+    nombre = input("nombre del almacen: ")
+    for y in almacen:
+        aux1 = True
+        while aux1:
+            if nombre in y:
+                nombre = input("nombre de almacen repetido, por favor cambie: ")
+            else:
+                aux1 = False        
+    item = []
+    for j in range(5):
+        items = []
+        producto = input("nombre del producto: ")
+        for y in item:
+            aux1 = True
+            while aux1:
+                if producto in y:
+                    producto = input("producto repetido, por favor cambie: ")
+                else:
+                    aux1 = False
+        items.append(int(input("cantidad vendida: ")))
+        items.append(int(input("precio por unidad: ")))
+        item.append({producto:items})
+    for k in item:
+        for x in k:
+            p[x] = k[x]
+    almacen[nombre] = p 
 
-    vald = True
-    while vald:
-        km1 = int(input("Ingresa los km recorridos en total: "))
-        km2 = int(input("Ingresa los km recorridos como lider: "))
-        if km1 <= total and km2 < km1:
-            estadisticas.append(km1)
-            estadisticas.append(km2)
-            vald = False
-        else:
-            print(f"los km recorridos superan los km totales de la carre {total}")
-    ciclistas[nombre] = estadisticas
 
+print(almacen)
 
-val = max(ciclistas.items(), key=lambda x: x[1])[0]
+maxvengen = []
+maxven = []
 
-
-for item in ciclistas:
-    es = []
-    if ciclistas[item][1] <= 1800:
-        print(f"El ciclista {item} cobrara de bonificacion {ciclistas[item][0] * suldobacis} ya que solo recorrio como lider {ciclistas[item][1]}")
-        es.append(ciclistas[item])
-        es[0].append(ciclistas[item][0] * suldobacis)
-        ciclistas[item] = es[0]
-    else:
-        print(f"El ciclista {item} cobrara de bonificacion {((ciclistas[item][0] - ciclistas[item][1]) * suldobacis) + (ciclistas[item][1] * ((suldobacis * 0.25)+ suldobacis))} ya que recorrio como lider {ciclistas[item][1]} y tinen una bonificacion del 25% por km recorrido como lider")
-        es.append(ciclistas[item])
-        es[0].append(((ciclistas[item][0] - ciclistas[item][1]) * suldobacis) + (ciclistas[item][1] * ((suldobacis * 0.25)+ suldobacis)))
-        ciclistas[item] = es[0]
-
-print(ciclistas)
-
-if ciclistas[val][0] == total:
-    ciclistas[item][2] = ciclistas[val][2] + 700000000 
-    print(f"El ganador es el {val} ya que termino la carrera y la cantidad de  y su sueldo es de {ciclistas[val][2]} ya que son de más 700 millones al ganador")
-
-print("----tebla de estadisticas------")
-for item in ciclistas:
+for item in almacen:
+    maxvengen.append([almacen[item][max(almacen[item].items(), key=lambda x: x[1])[0]][0], max(almacen[item].items(), key=lambda x: x[1])[0] ])
     print(f"""
+        Almacen: {item}
+        Articulo mas vendido: {max(almacen[item].items(), key=lambda x: x[1])[0]}
+        Unidades vendidas de ese articulo: {almacen[item][max(almacen[item].items(), key=lambda x: x[1])[0]][0]}
+        Precio por unidad {almacen[item][max(almacen[item].items(), key=lambda x: x[1])[0]][1]}
+        
+    """)
 
-        Nombre: {item}
-        kilometros totales recorridos: {ciclistas[item][0]}
-        Kilometros recorridos como lider: {ciclistas[item][1]}
-        Sueldo: {ciclistas[item][2]}
--------------------------------------------------------------------
+
+
+for item in almacen:
+    aux = 0
+    for valor in almacen[item]:
+        aux += almacen[item][valor][0]*almacen[item][valor][1]
+    maxven.append([aux , item])    
+
+
+
+print(f"""
+    El alamcen que mas vendio fue {max(maxven)[1]} con un total de {max(maxven)[0]}
+    El producto mas vendido en general es {max(maxvengen)[1]} con un total de {max(maxvengen)[0]} unidades vendidas
     """)
