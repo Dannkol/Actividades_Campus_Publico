716df225ac76b083d082562c4cb6350903a68d1d
Daniel <manosalvasierra@gmail.com>
2023-02-16 23:53:01 -0500

    feat taller 2 punto 3

--- taller_2/main.py
+++ taller_2/main.py
@@ -1,19 +1,58 @@
-atleta = {
+ciclistas = {
 
 }
+suldobacis = int(input("suldo basico por kilometro recorrido: "))
 
-record = 15.50
+total = 3277
 
-n = int(input("Cuantos atletas participan?: "))
+n = int(input("cuantos ciclistas participan: "))
 
 for i in range(n):
     nombre = input("nombre del atleta: ")
-    salto = int(input("distancia en metros del salto: "))
-    atleta[nombre] = salto
+    estadisticas = []
 
-val = max(atleta.items(), key=lambda x: x[1])[0]
+    vald = True
+    while vald:
+        km1 = int(input("Ingresa los km recorridos en total: "))
+        km2 = int(input("Ingresa los km recorridos como lider: "))
+        if km1 <= total and km2 < km1:
+            estadisticas.append(km1)
+            estadisticas.append(km2)
+            vald = False
+        else:
+            print(f"los km recorridos superan los km totales de la carre {total}")
+    ciclistas[nombre] = estadisticas
 
-if atleta[val] > record:
-    print(f"El atleta ganador es: {val} con un salto de {atleta[val]} metros, es mayor que el record actual por ende gana 500 millones")
-else:
-    print(f"El atleta ganador es: {val} con un salto de {atleta[val]} metros, pero no es mayor al record axtual")
+
+val = max(ciclistas.items(), key=lambda x: x[1])[0]
+
+
+for item in ciclistas:
+    es = []
+    if ciclistas[item][1] <= 1800:
+        print(f"El ciclista {item} cobrara de bonificacion {ciclistas[item][0] * suldobacis} ya que solo recorrio como lider {ciclistas[item][1]}")
+        es.append(ciclistas[item])
+        es[0].append(ciclistas[item][0] * suldobacis)
+        ciclistas[item] = es[0]
+    else:
+        print(f"El ciclista {item} cobrara de bonificacion {((ciclistas[item][0] - ciclistas[item][1]) * suldobacis) + (ciclistas[item][1] * ((suldobacis * 0.25)+ suldobacis))} ya que recorrio como lider {ciclistas[item][1]} y tinen una bonificacion del 25% por km recorrido como lider")
+        es.append(ciclistas[item])
+        es[0].append(((ciclistas[item][0] - ciclistas[item][1]) * suldobacis) + (ciclistas[item][1] * ((suldobacis * 0.25)+ suldobacis)))
+        ciclistas[item] = es[0]
+
+print(ciclistas)
+
+if ciclistas[val][0] == total:
+    ciclistas[item][2] = ciclistas[val][2] + 700000000 
+    print(f"El ganador es el {val} ya que termino la carrera y la cantidad de  y su sueldo es de {ciclistas[val][2]} ya que son de más 700 millones al ganador")
+
+print("----tebla de estadisticas------")
+for item in ciclistas:
+    print(f"""
+
+        Nombre: {item}
+        kilometros totales recorridos: {ciclistas[item][0]}
+        Kilometros recorridos como lider: {ciclistas[item][1]}
+        Sueldo: {ciclistas[item][2]}
+-------------------------------------------------------------------
+    """)
