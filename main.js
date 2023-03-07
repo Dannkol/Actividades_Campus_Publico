fd2db065d4de1ceb08e4dde93a00534b2b86f4a4
Daniel <manosalvasierra@gmail.com>
2023-03-07 15:53:53 -0500

    feat: :sparkles: .charAt()
    Un ejemplo del uso del metodo charAt

---
+++ main.js
@@ -0,0 +1,14 @@
+const cadena = String.raw = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+console.log(`
+
+    Metodos de strings en javascript
+
+    El método charAt(index) de String devuelve en un nuevo String el carácter UTF-16 de una cadena.
+
+    para la cadena: ${cadena} 
+    
+    su .charAt(index) en la poscion 0 es
+
+    ${cadena.charAt(0)}
+
+`)
