c48e44edf9df6bb7a8f99002bd60231ab1a0e2d5
Daniel <manosalvasierra@gmail.com>
2023-03-07 15:59:32 -0500

    feat: :sparkles: .charCodeAt(index)
    Un ejemplo para .charCodeAt(index)

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ console.log(`
 
     Metodos de strings en javascript
 
-    El método charAt(index) de String devuelve en un nuevo String el carácter UTF-16 de una cadena.
+    El método charCodeAt() devuelve un número entero entre 0 y 65535 que representa la unidad de código UTF-16 en el índice dado.
 
     para la cadena: ${cadena} 
     
-    su .charAt(index) en la poscion 0 es
+    su .charCodeAt(index) en la poscion 0 es
 
-    ${cadena.charAt(0)}
+    ${cadena.charCodeAt(0)}
 
 `)
