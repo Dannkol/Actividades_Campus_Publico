1332cd4653d47754f250ce8840384a634f445ae7
Daniel <manosalvasierra@gmail.com>
2023-03-07 16:25:56 -0500

    feat: :sparkles:.localeCompare()
    Un ejemplo de .localeCompare()

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ console.log(`
 
     Metodos de strings en javascript
 
-    El método search() ejecuta una búsqueda que encaje entre una expresión regular y el objeto String desde el que se llama.
-    
-    para la cadena: ${cadena} 
-    
-    su .search(regex) para la regex /Lorem/i
+    El método localeCompare() compara dos cadenas en la configuración regional actual.
 
-    ${cadena.search(/Lorem/i)}
+    El método localeCompare() devuelve el orden de clasificación -1, 1 o 0 (para antes, después o igual).
+
+    -1 si se ordena antes
+    1 si se ordena después
+    0 si es igual
+
+    El método localeCompare() retorna un número indicando si una cadena de carateres de referencia va antes, después o si es la misma que la cadena dada en orden alfabético.
+
+    La letra "a" va antes que "c" por lo que entrega un valor negativo
+    
+    'a'.localeCompare('c')
+
+    ${'a'.localeCompare('c')}
 
 `)
+
