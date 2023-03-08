542c4913e471a37c324f6b1a19a8544f34aa9348
Daniel <manosalvasierra@gmail.com>
2023-03-07 20:33:15 -0500

    feat: :sparkles: .repeat()
    Un ejemplo de .repeat()

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
 const cadena = String.raw = 'Lorem ipsum dolor sit Amet consectetur adipisicing elit.'
 console.log(`
 
-    Metodos de strings en javascript
-
-    El método match() devuelve todas las ocurrencias de una expresión regular dentro de una cadena.
-    
-    El método matchAll() retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, incluyendo grupos de captura.
-
-    para la cadena de texto
+    El método repeat() construye y devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados.
     
     ${cadena}
 
-    .match(/[A-Z]/g) -> me encontra todas las ocurrencias en el string que concidan con la exprecion regular
-    en este caso de la A a la Z (mayusculas)
+    .repeat(2) -> repite el string las veces del num en este caso 2 
 
-    ${cadena.match(/[A-Z]/g)}
-
-    .matchAll(/[A-Z]/g) -> devolvera un obajeto iterable [object RegExp String Iterator]
-
-    [...cadena.matchAll(/[A-Z]/g)]
-
-    ${[...cadena.matchAll(/[A-Z]/g)][0]}
-
+    ${cadena.repeat(2)}
 
 `)
 
