4afeac8af83394d0509cfa5efbbb11dc2d6728f0
daniel manosalva <manosalvasierra@gmail.com>
2023-03-08 13:18:31 -0500

    feat: 🚑 Operadores en javascript
    Un escrito por consola de los tipos de operadores en js (operadores logicos, aritmeticos, comparacion, operador ternario)
    
    Daniel Manosalva Taller 2 de javascript tipos de operadores

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log(
+    `
+
+    los operadores aritmeticos son : 
+
+        operadores aritméticos binarios son:
+        
+            * Operador suma: +.
+            * Operador resta: -.
+            * Operador producto: * (¡ojo! Utilizamos el asterisco y no la x).
+            * Operador división: /.
+            * Operador módulo o resto: % (¡cuidado! En programación no utilizamos el % para operaciones con porcentajes).
+            * Operador de exponenciación **.
+        
+            operadores aritméticos unarios son:
+
+                * Operador incremento: ++.
+                * Operador decremento: --.
+
+        operadores de comparación son:
+
+            * Mayor que > .
+            * Menor que <.
+            * Mayor e igual que <=.
+            * Menor e igual que >=.
+            * No es igual que !=.
+            * Estrictamente igual que ===.
+            * Igual que ==.
+            * Desigualdad estricta !==.
+        
+        operadores lógicos son:
+
+
+            * AND && : devuelve true (cierto) si los dos operandos son verdaderos.
+            * OR || : devuelve true (cierto) si al menos un operador es verdadero.
+            * NOT ! : solo se aplica a un operando. Devuelve true (cierto) si su operando es falso y false (falso) si su operando es cierto.
+
+            Operadores bit a bit:
+                
+                * AND a nivel de bits   a & b 
+                * OR a nivel de bits 	a | b
+                * XOR a nivel de bits   a ^ b
+                * NOT a nivel de bits   ~ a
+
+
+        Operadores de asignación:
+
+            * Asignación x = y.
+            * Asignación de adición x += y.
+            * Asignación de resta x -= y.
+            * Asignación de multiplicación x *= y.
+            * Asignación de división x /= y.
+            * Asignación de residuo x %= y.
+            * Asignación de exponenciación x **= y.
+            * Asignación de desplazamiento a la izquierda 	x <<= y  significa	x = x << y
+            * Asignación de desplazamiento a la derecha 	x >>= y  significa	x = x >> y
+            * Asignación de desplazamiento a la derecha sin signo 	x >>>= y  significa	x = x >>> y
+            * Asignación AND lógico 	        x &&= y   significa   x && (x = y)
+            * Asignación OR lógico 	            x ||= y   significa   x || (x = y)
+            * Asignación de anulación lógica 	x ??= y   significa	  x ?? (x = y)
+        
+
+        Operador condicional (ternario) es:
+
+            * condition ? val1 : val2 -> El operador puede tener uno de dos valores según una condición val1 si es true y val2 si es false. 
+
+    `
+)
