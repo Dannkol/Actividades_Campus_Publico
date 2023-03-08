bf3e6c4aaf06772c625413680b960e1daf518668
Daniel <manosalvasierra@gmail.com>
2023-03-07 20:52:14 -0500

    feat: :sparkles: .raw
    Un ejemplo de  .raw

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
 const cadena = String.raw = 'Lorem ipsum dolor sit Amet consectetur adipisicing elit.'
 console.log(`
 
-    El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada.
-
-    Para ${cadena} es
-
-    ${cadena.toLowerCase()}
-
-    El método toUpperCase() devuelve el valor de la cadena de llamada convertido a mayúsculas (el valor se convertirá en una cadena si no lo es).
-
-    Para ${cadena} es
-
-    ${cadena.toUpperCase()}
+    .raw() se utiliza para obtener un string crudo a partir de plantillas de string (es decir, el original, texto no interpretado).
+    
 
 `)
 
