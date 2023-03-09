a5ca736ee62fcec8cab4a3b455ad59aae9ba4257
Daniel Manosalva <manosalvasierra@gmail.com>
2023-03-09 12:57:08 -0500

    feat: :sparkles: Ejercicio 2 taller 2 de js
    Dado un número indicar si es par o impar y si es mayor de 10.
    
    echo con amor por dannkol <3

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
-let promp = 0;
-let nota = [];
-let x = true;
-
-function aggnota() {
-  let aux;
-  aux = parseInt(prompt("ingrese la nota del alumno: "));
-  do {
-    if (aux > 5) {
-      aux = parseInt(prompt("ingrese la nota del alumno menor a 5: "));
-    } else {
-      break;
-    }
-  } while (true);
-  return aux;
-}
+let num = 0;
 
 do {
   try {
-    prompt(
-      "quiere agregar una nota escriba si o cualquier otra cosa para cancelar"
-    ).toLocaleLowerCase() == "si"
-      ? nota.push(aggnota())
-      : (x = false);
+    num = parseInt(
+      prompt("ingrese un numero para saber si es par o impar y es mayor a 10")
+    );
   } catch (error) {
     alert(`Dato no conocido : ${error}`);
   }
-} while (x);
 
-try {
-  promp = nota.reduce((acc, cur) => (cur += acc)) / nota.length;
+  alert(
+    `${num % 2 == 0 ? "El numbre es par" : "El numero no es par"} y ${
+      num > 10 ? "el numero es mayor a 10" : "el numero es menor a 10"
+    }`
+  );
 
-  promp >= 3.9
-    ? alert(`Becado con un promedio de ${promp.toFixed(2)}`)
-    : alert(`Estudie su promedio es de ${promp.toFixed(2)}`);
-} catch (error) {
-    alert(`Datos incorrectos: ${error}`)
-    window.location.reload()
-}
+    if(!confirm("¿Desea Continuar?")){
+      break
+    }
+
+} while (true);
