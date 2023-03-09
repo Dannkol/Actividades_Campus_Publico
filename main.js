05ce8cb8ddb00ced9c86b16b553345c56f309e77
Daniel Manosalva <manosalvasierra@gmail.com>
2023-03-09 13:30:51 -0500

    feat: :sparkles: Ejercico 4 taller 2 de js
    Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona con mayor edad.
    
    echo con amor por dannkol <3

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-let res = 0;
-let int = 0;
+let persona = {
+  personas: [],
+  edades: [],
+};
 
 do {
   try {
-    res = parseFloat(prompt("ingrese la resistencia del circuito en ohm"));
-    int = parseFloat(prompt("ingrese la corriente del circuito en ampers A"));
+    persona.personas.push(prompt("ingrese el nombre de la persona"));
+    persona.edades.push(parseInt(prompt("ingrese la edad de la persona")));
   } catch (error) {
     alert(`Dato no conocido : ${error}`);
   }
-
-  alert(
-    `El voltaje del circuito es ${res * int}`
-  );
 
   if (!confirm("¿Desea Continuar?")) {
     break;
   }
 } while (true);
+
+alert(
+  `la persona con mayor edad es ${
+    persona.personas[persona.edades.indexOf(Math.max(...persona.edades))]
+  } con ${Math.max(...persona.edades)}`
+);
