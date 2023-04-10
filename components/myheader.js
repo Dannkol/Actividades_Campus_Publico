e2a87a7bb49587d235b50f3c2c807623dee1ad43
Daniel Manosalva <manosalvasierra@gmail.com>
2023-04-10 09:26:55 -0500

    feat: :sparkles: primer intento de dom

--- components/myheader.js
+++ components/myheader.js
@@ -14,12 +14,20 @@ export default {
         console.log(data)
         console.log(JSON.parse(data[1])) */
         const mydata =  document.querySelector("#myformdata");
+        const egresos = document.querySelector("#egresos");
         mydata.addEventListener("submit", (e)=>{
             e.preventDefault() 
             let data = Object.fromEntries(new FormData(e.target));
             const datos_local = config.localstoragedata("datos_ingresos" , JSON.stringify(data))
-            const myworker = new Worker("components/worker/calcular.js")
+            const myworker = new Worker("components/worker/calcular.js", {type : "module"})
             myworker.postMessage({data : datos_local})
+            myworker.addEventListener("message", (e)=>{
+                egresos.innerHTML = ''
+                console.log(e.data)
+                let doc = new DOMParser().parseFromString(e.data[2], "text/html");
+                console.log(...doc.body.children)
+                egresos.appendChild(...doc.body.children);
+            })
         })  
     }
 }
--- components/worker/calcular.js
+++ components/worker/calcular.js
@@ -31,16 +31,94 @@
 
 */
 
+const opter = {
+  suma(data) {
+    return data.reduce((acc, item) => acc + parseFloat(item.valor), 0);
+  },
+
+  resta(data) {
+    return data.reduce((acc, item) => acc - parseFloat(item.valor), 0);
+  },
+  porcentaje_egresos(data, resta, suma) {
+    console.log(resta , suma);
+    let plantilla = "";
+    plantilla = ` 
+      <div class="card-body d-flex justify-content-between bg-danger p-1"><p class="m-0 p-2">EGRESOS</p>
+      <p class="m-0 p-2">${data.reduce(
+        (acc, item) => acc + parseFloat(item.valor),
+        0
+      )}</p>
+      <div
+        class="alert text-center alert-danger p-0 m-0 fs-6 w-auto h-50"
+        role="alert"
+      >
+      ${((resta * 100) / Math.abs(suma - resta)).toFixed(2)}%
+      </div></div>
+    `;
+    return plantilla;
+  },
+  porcentaje_egresos_item(data) {
+    data.forEach((item) => console.log(item));
+
+    let plantilla = "";
+
+    plantilla += data
+      .map((element, id) => {
+        return `<tr>
+          <td class="data_table">
+            <div
+              class="delet_animation card d-flex flex-row flex-nowrap justify-content-center  border-0 bg-transparent"
+            >
+              <div
+                class=" d-flex  justify-content-between align-content-center p-0"
+                style="width: 260px"
+              >
+                <p class="m-0 p-2">${element.Descripcion}</p>
+                <p class="m-0 p-2">${parseFloat(element.valor)}</p>
+                <div
+                  class="alert text-center alert-danger p-0 m-0 fs-6 w-auto h-75"
+                  role="alert"
+                >
+                ${(
+                  (parseFloat(element.valor) * 100) /
+                  data.reduce((acc, item) => acc + parseFloat(item.valor), 0)
+                ).toFixed(2)}%
+                </div>
+                
+              </div>
+              <div class="icon_close w-25 h-auto " id="${id}">
+                <span class=" material-symbols-outlined"> close </span>
+              </div>
+            </div>
+          </td>
+        </tr>
+        `;
+      })
+      .join("");
+    console.log(plantilla);
+    return plantilla;
+  },
+  total(ingresos, egresos) {
+    return ingresos + egresos;
+  },
+};
+
+const draw_data = {};
+
 self.addEventListener("message", (e) => {
   let data = JSON.parse(e.data.data);
   data = data.map((item) => JSON.parse(item));
-  let resta = data
-    .filter((itme) => itme.tipo_gasto === "-")
-    .reduce((acc, item) => acc + parseFloat(item.valor), 0);
-  let suma = data
-    .filter((itme) => itme.tipo_gasto === "+")
-    .reduce((acc, item) => acc + parseFloat(item.valor), 0);
+  let data_egresos = data.filter((itme) => itme.tipo_gasto === "-");
+  let data_ingresos = data.filter((itme) => itme.tipo_gasto === "+");
+  let total = opter.total(opter.resta(data_egresos), opter.suma(data_ingresos));
 
-  console.log(resta);
-  console.log(suma);
+  postMessage([
+    opter.resta(data_egresos),
+    opter.suma(data_ingresos),
+    opter.porcentaje_egresos(
+      data_egresos,
+      opter.resta(data_egresos),
+      opter.suma(data_ingresos)
+    ),
+  ]);
 });
