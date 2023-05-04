54c1cf05c006a6dd124dc04c46fe3eefee042822
Daniel Manosalva <Manosalvasierra@gmail.com>
2023-05-04 08:19:01 -0500

    feat: :rocket: final actividad 1

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import myHeader from "./componets/my-header/my-header.js";
+import mySection from "./componets/my-section/my-section.js";
+import myNav from "./componets/my-nav/my-nav.js";
+import myFooter from "./componets/my-footer/my-footer.js";
+import myChechks from "./componets/my-checks/my-select.js";
 
 myHeader.componet()
+mySection.componet()
+myNav.componet()
+myChechks.componet()
+myFooter.componet()
