c8d6339bb1c3dec44c42a0351935e0fd095a88fa
Anderson Cespedes <andersontrabajos2@gmail.com>
2023-05-23 14:01:06 -0500

    feat: :sparkles: taller php punto 7
    Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
 let myFormulario = document.querySelector("#myFormulario");
-const result = document.querySelector("#result");
-const envio = document.querySelector("#favDialog");
-const cancel = document.querySelector("#cancel");
-const modal = document.querySelector("#modal");
 
-let datos = [];
 
 myFormulario.addEventListener("submit", async (e) => {
   e.preventDefault();
   let data = Object.fromEntries(new FormData(e.target));
-  datos.push(data);
-  envio.showModal();
-});
-
-cancel.addEventListener("click", (e) => {
-    envio.close();
-});
-
-
-modal.addEventListener("submit", async (e) => {
+  console.log(data);
   let res = await (
     await fetch("api.php", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(datos),
+      body: JSON.stringify(data),
     })
   ).json();
-  result.innerHTML = `<h1>resultado</h1><div>
+  console.log(res);
+  result.innerHTML = `<h1>Factura</h1><div>
   
-    <h4>cantidad de mujeres: ${res.mujeres}</h4>
-    <h4>cantidad de hombres: ${res.hombres}</h4>
-    <p>menor nota </p>
-    <ul>
-        <li>nombre : ${res.result[0][0]}</li>
-        <li>nota : ${res.result[0][1]}</li>
-    </ul>
-    <p>mayor nota </p>
-    <ul>
-        <li>nombre : ${res.result[1][0]}</li>
-        <li>nota : ${res.result[1][1]}</li>
-    </ul>
-
+    <h4>nombre del producto: ${res.nom_produc}</h4>
+    <h4>precio a pagar: ${res.factura}</h4>
+    
   </div>`;
 });
+
+
--- requets.php
+++ requets.php
@@ -3,44 +3,12 @@
     
 
     function reqpost($_DATA){
-     if(validarNumerosEnArray($_DATA)){
-
-        $cantidadobj = count($_DATA);
-
-        $mujeres = 0;
-
-        foreach ($_DATA as $objeto) {
-            if ($objeto['sexo'] === 'femenino') {
-                $mujeres++;
-            }
-        }
-
-        // Inicializar las variables para la menor y mayor nota
-        $mayorNota = PHP_INT_MIN;
-        $nombreMayor = '';
-        $menorNota = PHP_INT_MAX;
-        $nombreMenor = '';
-        // Iterar sobre los objetos y encontrar la menor y mayor nota
-        foreach ($_DATA as $objeto) {
-          $nota = intval($objeto['nota']);
-
-          if ($nota < $menorNota) {
-            $menorNota = $nota;
-            $nombreMenor = $objeto['nombre'];
-          }
-
-          if ($nota > $mayorNota) {
-            $mayorNota = $nota;
-            $nombreMayor = $objeto['nombre'];
-          }
-        }
+      if(validarNumerosEnArray($_DATA)){
 
         $_Respuesta =  (array) [
             "datos" => $_DATA,
-            "mujeres" => $mujeres,
-            "hombres" => $cantidadobj - $mujeres,
-            "result" => array([$nombreMenor , $menorNota],[$nombreMayor ,$mayorNota] ),
-            "cantidad" => $cantidadobj
+            "nom_produc" => $_DATA['nom_prduc'],
+            "factura" => $_DATA['cantidad'] * $_DATA['precio'],
         ];
         }else{
             $_Respuesta = error('Datos no permitidos');
